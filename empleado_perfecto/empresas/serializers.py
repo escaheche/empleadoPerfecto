43d82d774c1f322852c99642fb1b25e108eb9dd1
Felipe Martínez <felipe.martinez42@inacapmail.cl>
2024-09-26 02:40:24 -0300

    se agrega encriptacion de la pass

--- empleado_perfecto/empresas/serializers.py
+++ empleado_perfecto/empresas/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from .models import Empresa, Empleado
 
@@ -11,11 +12,17 @@ class EmpleadoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Empleado
-        fields = ['id', 'nombre_completo', 'rut', 'email', 'empresa']
+        fields = ['id', 'nombre_completo', 'rut', 'email', 'password', 'empresa']  # Asegúrate de incluir el campo 'password'
 
     def to_representation(self, instance):
         # Para la representación (respuesta), utilizamos el serializer de Empresa completo
         representation = super().to_representation(instance)
         representation['empresa'] = EmpresaSerializer(instance.empresa).data  # Muestra los detalles de la empresa
         return representation
+
+    def create(self, validated_data):
+        # Encripta la contraseña antes de crear el empleado
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
 
